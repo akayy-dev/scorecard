24e59177e30bb711910500e7e21392cd3fe57174
Ahadu Kebede <ahadukebede@gmail.com>
2022-09-12 08:25:03 -0400

    Added year parameter.

--- scorecard/api.py
+++ scorecard/api.py
@@ -7,14 +7,14 @@ class ScoreCard:
 	def __init__(self, API_KEY: str) -> None:
 		self.API_KEY = API_KEY
 
-	def search(self, name: str, page=0, per_page=20) -> College:
+	def search(self, name: str, page=0, per_page=20, year='latest') -> College:
 		"""Search for a college by name."""
 		params = {
 			'api_key': self.API_KEY,
 			'school.name': name,
 			'page': page,
 			'per_page': per_page,
-			'_fields': 'school,id'
+			'_fields': f'{year}.school,{year}.student,id'
                     }
 		r = requests.get(
 			'https://api.data.gov/ed/collegescorecard/v1/schools.json', params=params)
@@ -22,15 +22,15 @@ class ScoreCard:
 
 		college_list = []
 		for i in results:
-			college_list.append(College(data=i))
+			college_list.append(College(data=i, year=year))
 
 		return college_list
 
-	def get_by_id(self, id: int):
+	def get_by_id(self, id: int, year='latest'):
 		"""Query a college by it's id."""
 		params = {
 			'api_key': self.API_KEY,
-			'_fields': 'school,id',
+			'_fields': f'{year}.school,{year}.student,id',
 			'id': id,
                     }
 		r = requests.get(
@@ -40,4 +40,4 @@ class ScoreCard:
 		# So I need to append the [0] to select the first result.
 		results = r.json()['results'][0]
 
-		return College(data=results)
+		return College(data=results, year=year)
--- scorecard/objects.py
+++ scorecard/objects.py
@@ -1,19 +1,20 @@
 class College:
-	def __init__(self, data: list) -> None:
+	def __init__(self, data: list, year: str) -> None:
 		self.data = data
+		self.year = year
 
 	@property
 	def name(self) -> str:
 		"""The name of the college."""
-		return self.data['school.name']
+		return self.data[f'{self.year}.school.name']
 
 	@property
 	def location(self) -> dict:
 		"""Return the state and city of the college."""
-		return {'city': self.data['school.city'], 'state': self.data['school.state'], 'zip': self.data['school.zip']}
+		return {'city': self.data[f'{self.year}.school.city'], 'state': self.data[f'{self.year}.school.state'], 'zip': self.data[f'{self.year}.school.zip']}
 
 	def __str__(self) -> str:
-		return f'{self.data["school.name"]} - {self.data["id"]}'
+		return f'{self.data[f"{self.year}.school.name"]} - {self.data[f"id"]}'
 
 	def __eq__(self, other: object) -> bool:
 		if self.data['id'] == other.data['id']:
