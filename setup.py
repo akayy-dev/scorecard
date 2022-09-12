67c0c9fe681c6d920b9c5140e3cb18cb3632dcd6
Ahadu Kebede <ahadukebede@gmail.com>
2022-09-12 12:44:58 -0400

    Added `setup.py`

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+	name='scorecard',
+	description='A python library for collegescorecard.ed.gov',
+	author='Ahadu Kebede',
+	author_email='ahadukebede@gmail.com',
+	url='https://github.com/ahoodatheguy/scorecard',
+	packages=['scorecard']
+)
